ca2ab956419e463f81a9d3c5e356ec145db14629
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2024-04-23 15:12:42 +0100

    Layer attempt

--- src/services/logger.rs
+++ src/services/logger.rs
@@ -1,3 +1,8 @@
+use std::{
+    io::Write,
+    sync::{Arc, Mutex},
+};
+
 use super::config::get_data_dir;
 use anyhow::Result;
 use tracing_error::ErrorLayer;
@@ -9,17 +14,19 @@ lazy_static::lazy_static! {
   pub static ref LOG_FILE: String = format!("{}.log", env!("CARGO_PKG_NAME"));
 }
 
-pub fn initialize_logging() -> Result<()> {
+pub fn initialize_logging(writer: VecWriter) -> Result<()> {
     let directory = get_data_dir()?;
     std::fs::create_dir_all(directory.clone())?;
     let log_path = directory.join(LOG_FILE.clone());
     let log_file = std::fs::File::create(log_path)?;
+
     std::env::set_var(
         "RUST_LOG",
         std::env::var("RUST_LOG")
             .or_else(|_| std::env::var(LOG_ENV.clone()))
             .unwrap_or_else(|_| format!("{}=info", env!("CARGO_CRATE_NAME"))),
     );
+
     let file_subscriber = tracing_subscriber::fmt::layer()
         .with_file(true)
         .with_line_number(true)
@@ -27,8 +34,18 @@ pub fn initialize_logging() -> Result<()> {
         .with_target(false)
         .with_ansi(false)
         .with_filter(tracing_subscriber::filter::EnvFilter::from_default_env());
+
+    let console_suscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_writer(writer)
+        .with_target(false)
+        .with_ansi(false)
+        .with_filter(tracing_subscriber::filter::EnvFilter::from_default_env());
+
     tracing_subscriber::registry()
         .with(file_subscriber)
+        .with(console_suscriber)
         .with(ErrorLayer::default())
         .init();
     Ok(())
@@ -60,32 +77,32 @@ macro_rules! trace_dbg {
     };
 }
 
-// pub type Logs = Arc<Mutex<Vec<String>>>;
-//
-// pub struct VecWriter {
-//     logs: Logs,
-// }
-//
-// impl Write for VecWriter {
-//     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-//         let msg = String::from_utf8_lossy(buf);
-//         let mut logs = self.logs.lock().unwrap();
-//
-//         logs.push(msg.into_owned());
-//
-//         Ok(buf.len())
-//     }
-//
-//     fn flush(&mut self) -> std::io::Result<()> {
-//         Ok(())
-//     }
-// }
-//
-// impl VecWriter {
-//     pub fn new(logs: Logs) -> Self {
-//         Self { logs }
-//     }
-// }
+pub type Logs = Arc<Mutex<Vec<String>>>;
+
+pub struct VecWriter {
+    logs: Logs,
+}
+
+impl Write for VecWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let msg = String::from_utf8_lossy(buf);
+        let mut logs = self.logs.lock().unwrap();
+
+        logs.push(msg.into_owned());
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
+
+impl VecWriter {
+    pub fn new(logs: Logs) -> Self {
+        Self { logs }
+    }
+}
 //
 // pub fn init_logging(writer: VecWriter, level: LevelFilter) {
 //     // let data_dir = get_data_dir().unwrap();
