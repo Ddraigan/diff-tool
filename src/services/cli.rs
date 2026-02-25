5023494178336faa79cb359e963e74193649227a
Leon Jones <Leon@rnw.local>
2026-02-25 16:29:14 +0000

    Minor improvements

--- src/services/cli.rs
+++ src/services/cli.rs
@@ -6,10 +6,11 @@ use crate::services::config::{get_config_dir, get_data_dir};
 #[derive(Parser, Debug)]
 #[command(author = "Ddraigan", version = version(), about = "A side by side git diff view")]
 pub struct Args {
-    #[clap(short = 'C', long)]
+    #[arg(short = 'C', long)]
     /// Giving a full path allows diff-tool to diff outside of the git repo
     change_dir: bool,
     /// File to diff with
+    #[arg(value_name = "FILE")]
     path: PathBuf,
     // #[clap(short, long, default_value_t = 250)]
     // tick_rate: u64,
@@ -29,14 +30,19 @@ impl Args {
 pub fn version() -> clap::builder::Str {
     let author = clap::crate_authors!();
 
-    let commit_hash = env!("DIFF_TOOL_GIT_INFO");
+    let commit_hash = option_env!("DIFF_TOOL_GIT_INFO").unwrap_or("unknown");
 
-    let config_dir_path = get_config_dir().unwrap().display().to_string();
-    let data_dir_path = get_data_dir().unwrap().display().to_string();
+    let config_dir_path = get_config_dir()
+        .map(|p| p.display().to_string())
+        .unwrap_or_else(|_| "Unavailable".into());
+
+    let data_dir_path = get_data_dir()
+        .map(|p| p.display().to_string())
+        .unwrap_or_else(|_| "Unavailable".into());
 
     format!(
-        "\
-    {commit_hash}
+        "\n
+    Commit: {commit_hash}
 
     Authors: {author}
 
