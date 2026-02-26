17e6b2d6f34e4942e8bee3aab9c1d61c4a0bbe63
Leon Jones <Leon@rnw.local>
2026-02-26 13:19:15 +0000

    probably shouldn't have been using 'str' as a var name

--- src/update/message.rs
+++ src/update/message.rs
@@ -14,13 +14,13 @@ pub enum Message {
 /// Display a user friendly short description of action
 impl Display for Message {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let str = match self {
+        let description = match self {
             Message::PrevRow => "Scroll up one row",
             Message::NextRow => "Scroll down one row",
             Message::LastRow => "Jump to bottom row",
             Message::FirstRow => "Jump to top row",
             Message::Quit => "Quit application",
         };
-        write!(f, "{}", str)
+        write!(f, "{}", description)
     }
 }
